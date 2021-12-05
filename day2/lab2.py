73a31a8c3b5c9f3e015cd6d980f04e5981dc3a75
rabi <wrabwagle123@gmail.com>
2021-12-05 13:08:42 +0545

    second commit

---
+++ day2/lab2.py
@@ -0,0 +1,6 @@
+
+#BMI
+height=float(input("Enter your height (in metre): "))
+weight=float(input("Enter your weight (in kg): "))
+bmi=weight/(height**2)
+print("BMI: {} kg/m\u00b2".format(bmi))
---
+++ day2/lab4.py
@@ -0,0 +1,8 @@
+Abhyas Adhikari
+#apples in basket
+a=int(input("Enter number of apples in the basket: "))
+b=int(input("Enter number of students: "))
+c=a//b
+print("{} apples for single student".format(c))
+d=a-(c*b)
+print("{} apples remain in basket".format(d))
